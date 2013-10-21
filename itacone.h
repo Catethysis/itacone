c73b339c99ae0539273666e5def85a5cb0d428a7
catethysis <catethysis@gmail.com>
2013-10-21 09:24:51 +0400

    Первые шаги к многим кристаллам
    Написано условное подключение файла конфигурации в зависимости от
    объявленного ядра.

--- itacone.h
+++ itacone.h
@@ -9,7 +9,13 @@ http://catethysis.ru/index.php/stm32-itacone-library/
 
 */
 
+//Название ядра кристалла
+#define stm32f100
+
+//Вспомогательные функции
 #include "inc/itacone_misc.h"
 
-//Файл конфигурации
-#include "conf/itacone_conf_stm32f100.h"
+//Выбираем файл конфигурации
+#ifdef stm32f100
+    #include "conf/itacone_conf_stm32f100.h"
+#endif
