9443715dcaf7c1e2e24a0e896b51477550091ca1
catethysis <catethysis@gmail.com>
2013-10-23 03:04:43 +0400

    Обновлён misc, начало работы над uart
    В модуль misc добавлены функции получения Unique_ID, начата работа над
    UART.

--- inc/itacone_misc.h
+++ inc/itacone_misc.h
@@ -8,3 +8,9 @@ void delayMs(int ms)
 {
   int n=(SystemCoreClock/1000/5)*ms; while(n-->0); //Почему 5?
 }
+
+uint16_t Unique_ID_Low0(void) { return *(u16*)(0x1FFFF7E8); };
+uint16_t Unique_ID_Low1(void) { return *(u16*)(0x1FFFF7EA); };
+uint32_t Unique_ID_Low (void) { return *(u32*)(0x1FFFF7E8); };
+uint32_t Unique_ID_Mid (void) { return *(u32*)(0x1FFFF7EC); };
+uint32_t Unique_ID_Hig (void) { return *(u32*)(0x1FFFF7F0); };
--- inc/itacone_uart.h
+++ inc/itacone_uart.h
@@ -4,18 +4,25 @@
 
 */
 
-/*class uart
+#define ENABLE_USART 1
+#define DISABLE_USART 0
+#define ENABLE_IT 1
+#define DISABLE_IT 0
+
+class uart
 {
 public:
   int number;
   
-  void USART1_Config(int divider, int enable_usart, int enable_it)
+  void config(int divider, int enable_usart, int enable_it)
   {
     RCC->APB2ENR  |= RCC_APB2ENR_USART1EN | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
     GPIOA->CRH	|= GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_0 | GPIO_CRH_CNF10_0;
     USART1->BRR = 0x9C4/divider; //Скорость обмена 19200 бод при 24MHz
     USART1->CR1   |= USART_CR1_TE | USART_CR1_RE;
-    if(enable_usart==ENABLE_USART) USART1_Enable();
+    if(enable_usart==ENABLE_USART) enable();
     if(enable_it==ENABLE_IT) { USART1->CR1 |= USART_CR1_RXNEIE; NVIC_EnableIRQ(USART1_IRQn); }
   }
-}*/
+  
+  void enable(){ USART1->CR1 |= USART_CR1_UE; }
+};
